51cf60c820c8f83f2a4210d162f90923cfadaead
chainzhang <zokei.chotei@gmail.com>
2014-06-05 17:30:21 +0900

    fix resources and add DynamicsLabel class

---
+++ Classes/DynamicsLabel.cpp
@@ -0,0 +1,9 @@
+//
+//  DynamicsLabel.cpp
+//  snowedge_test
+//
+//  Created by ChainZ on 2014/06/05.
+//
+//
+
+#include "DynamicsLabel.h"
---
+++ Classes/DynamicsLabel.h
@@ -0,0 +1,14 @@
+//
+//  DynamicsLabel.h
+//  snowedge_test
+//
+//  Created by ChainZ on 2014/06/05.
+//
+//
+
+#ifndef __snowedge_test__DynamicsLabel__
+#define __snowedge_test__DynamicsLabel__
+
+#include <iostream>
+
+#endif /* defined(__snowedge_test__DynamicsLabel__) */
